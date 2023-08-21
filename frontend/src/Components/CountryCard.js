5d8dd6075480588e310cb7e8750ba74b5e483794
Yashin <yashinnaran@gmail.com>
2023-08-21 13:31:52 +0200

    Created CountryCard.js component uses temp prop to populate component information

---
+++ frontend/src/Components/CountryCard.js
@@ -0,0 +1,27 @@
+import styles from "./CountryCard.module.css";
+
+const CountryCard = ({ country }) => {
+  return (
+    <div className={`col-4 ${styles.country_card} p-0`}>
+      <img className={styles.flag_img} src={country.flag} alt="" />
+      <div className={styles.details_container}>
+        <h2 className="">{country.name}</h2>
+        <p>
+          <strong> Population:</strong>
+          {country.population}{" "}
+        </p>
+        <p>
+          {" "}
+          <strong>Region:</strong> {country.region}
+        </p>
+        <p>
+          {" "}
+          <strong>Capital:</strong>
+          {country.capital}{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CountryCard;
