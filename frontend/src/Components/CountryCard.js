f0bb1844049eb60ae65418c9d6f29f8069bc755a
Yashin <yashinnaran@gmail.com>
2023-08-21 16:22:35 +0200

    Wrapped card component on div with col class inorder for boostrap grid system to work

--- frontend/src/Components/CountryCard.js
+++ frontend/src/Components/CountryCard.js
@@ -2,23 +2,25 @@ import styles from "./CountryCard.module.css";
 
 const CountryCard = ({ country }) => {
   return (
-    <div className={`col-4 ${styles.country_card} p-0`}>
-      <img className={styles.flag_img} src={country.flag} alt="" />
-      <div className={styles.details_container}>
-        <h2 className="">{country.name}</h2>
-        <p>
-          <strong> Population:</strong>
-          {country.population}{" "}
-        </p>
-        <p>
-          {" "}
-          <strong>Region:</strong> {country.region}
-        </p>
-        <p>
-          {" "}
-          <strong>Capital:</strong>
-          {country.capital}{" "}
-        </p>
+    <div className="col-3 p-0">
+      <div className={`${styles.country_card} p-0`}>
+        <img className={styles.flag_img} src={country.flag} alt="" />
+        <div className={styles.details_container}>
+          <h2 className="">{country.name}</h2>
+          <p>
+            <strong> Population:</strong>
+            {country.population}{" "}
+          </p>
+          <p>
+            {" "}
+            <strong>Region:</strong> {country.region}
+          </p>
+          <p>
+            {" "}
+            <strong>Capital:</strong>
+            {country.capital}{" "}
+          </p>
+        </div>
       </div>
     </div>
   );
