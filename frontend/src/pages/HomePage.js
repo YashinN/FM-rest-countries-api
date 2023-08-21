4daec3d0b3c4345831d51b7e4aa2261924ba12f9
Yashin <yashinnaran@gmail.com>
2023-08-21 16:29:31 +0200

    component maps through country data createing new CountryCard.js instances

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,8 +1,21 @@
-const HomePage = () => {
+import CountryCard from "../Components/CountryCard";
+import SearchBar from "../Components/SearchBar";
+import styles from "./HomePage.module.css";
+
+const HomePage = ({ TempData }) => {
+  const demoCountry = TempData[10];
+
   return (
-    <main id="light">
-      <section className="container">
-        <div className="row"></div>
+    <main>
+      <div className="container p-0">
+        <SearchBar />
+      </div>
+      <section className={`container ${styles.countries} p-0`}>
+        <div className="row m-0 gy-4">
+          {TempData.map((country) => (
+            <CountryCard country={country} />
+          ))}
+        </div>
       </section>
     </main>
   );
