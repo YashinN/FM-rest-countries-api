8ca2876af87ca8eb271ac09bf61458c90704e129
Yashin <yashinnaran@gmail.com>
2023-08-21 13:25:27 +0200

    Navbar component created

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -7,7 +7,6 @@ const Navbar = () => {
       <div className={styles.nav_container}>
         <h1 className={`${styles.nav_title}  mb-0`}>Where in the world?</h1>
         <button className={`${styles.theme_btn}`}>
-          {" "}
           <MoonIcon /> Dark Mode
         </button>
       </div>
