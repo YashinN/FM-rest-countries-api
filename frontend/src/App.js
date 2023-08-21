247f4ff58ab8f056de5a3eb810f6893386f5cebf
Yashin <yashinnaran@gmail.com>
2023-08-21 11:47:30 +0200

    Added temp json data and create homepage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
+import Navbar from "./Components/Navbar";
+import HomePage from "./pages/HomePage";
+import TempData from "./data.json";
+
 function App() {
   return (
-    <div className="App">
-      <div>hello</div>
-    </div>
+    <>
+      <Navbar />
+      <HomePage />
+    </>
   );
 }
 
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <main id="light">
+      <section className="container">
+        <div className="row"></div>
+      </section>
+    </main>
+  );
+};
+
+export default HomePage;
