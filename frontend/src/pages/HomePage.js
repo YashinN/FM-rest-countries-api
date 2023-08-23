b849058fd543f8e1517ab1fe6335fa2290b2f05e
Yashin <yashinnaran@gmail.com>
2023-08-23 17:21:45 +0200

    added temp state to handle country data and search state , with handle search function

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,18 +1,24 @@
+import { useEffect, useState } from "react";
 import CountryCard from "../Components/CountryCard";
 import SearchBar from "../Components/SearchBar";
 import styles from "./HomePage.module.css";
 
 const HomePage = ({ TempData }) => {
-  const demoCountry = TempData[10];
+  const [data, setData] = useState(TempData);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
 
   return (
     <main>
       <div className="container p-0">
-        <SearchBar />
+        <SearchBar onSearch={handleSearch} searchQuery={searchQuery} />
       </div>
       <section className={`container ${styles.countries} p-0`}>
         <div className="row m-0 gy-4">
-          {TempData.map((country) => (
+          {data.map((country) => (
             <CountryCard country={country} />
           ))}
         </div>
