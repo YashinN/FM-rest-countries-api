990da003c842d4479a52e984bda4f1f3792b9d72
Yashin <yashinnaran@gmail.com>
2023-08-23 17:24:04 +0200

    Search component sets search query on change

--- frontend/src/Components/SearchBar.js
+++ frontend/src/Components/SearchBar.js
@@ -1,13 +1,15 @@
 import styles from "./SearchBar.module.css";
 import SearchIcon from "../Icons/SearchIcon";
 
-const SearchBar = () => {
+const SearchBar = ({ onSearch, searchQuery }) => {
   return (
     <div className={styles.search_container}>
       <input
         className={styles.search_bar}
         type="text"
         placeholder="Search for a country..."
+        value={searchQuery}
+        onChange={(e) => onSearch(e.target.value)}
       />
 
       <span className={styles.icon_container}>
