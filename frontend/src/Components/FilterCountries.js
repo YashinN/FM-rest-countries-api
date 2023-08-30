0b44c69ac716ccd9f48507d07fa900679aba00f7
Yashin <yashinnaran@gmail.com>
2023-08-30 12:31:23 +0200

    added logic to open and close filter options

--- frontend/src/Components/FilterCountries.js
+++ frontend/src/Components/FilterCountries.js
@@ -1,26 +1,35 @@
 import styles from "./FilterCountries.module.css";
 
 import Chevron from "../Icons/Chevron";
+import { useState } from "react";
 
 const FilterCountries = () => {
+  const [openFilter, setOpenFilter] = useState(false);
+
+  const handleFilterState = () => {
+    setOpenFilter((state) => !state);
+  };
+
   return (
     <div className={styles.filter_wrapper}>
-      <div className={`${styles.filter_container}`}>
+      <div className={`${styles.filter_container}`} onClick={handleFilterState}>
         <p className="m-0">Filter by Region</p>
         <span>
           <Chevron />
         </span>
       </div>
 
-      <div className={`${styles.filter_items_container}`}>
-        <ul className={`p-0  ${styles.filter_items}`}>
-          <li>Africa</li>
-          <li>America</li>
-          <li>Asia</li>
-          <li>Europe</li>
-          <li>Oceania</li>
-        </ul>
-      </div>
+      {openFilter && (
+        <div className={`${styles.filter_items_container}`}>
+          <ul className={`p-0  ${styles.filter_items}`}>
+            <li>Africa</li>
+            <li>America</li>
+            <li>Asia</li>
+            <li>Europe</li>
+            <li>Oceania</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
