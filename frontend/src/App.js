d0b8e4161d612889dac6aa5fb2b3711a7f73b5e1
Yashin <yashinnaran@gmail.com>
2023-08-31 14:22:21 +0200

    Added navigation logic to App.js using react browser router , when a country is selected react navigates to the country page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,53 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import HomePage from "./pages/HomePage";
+import CountryPage from "./pages/CountryPage";
 import TempData from "./data.json";
 
 function App() {
+  const [countryDetails, setCountryDetails] = useState(null);
+
+  const handleCountryDetails = (country) => {
+    setCountryDetails(country);
+  };
+
   return (
     <>
-      <Navbar />
-      <HomePage TempData={TempData} />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              !countryDetails ? (
+                <HomePage
+                  TempData={TempData}
+                  handleCountryDetails={handleCountryDetails}
+                />
+              ) : (
+                <Navigate to="/country" />
+              )
+            }
+          />
+
+          <Route
+            path="/country"
+            element={
+              !countryDetails ? (
+                <Navigate to="/" />
+              ) : (
+                <CountryPage
+                  selectedCountry={countryDetails}
+                  setCountryDetails={setCountryDetails}
+                />
+              )
+            }
+          />
+
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
