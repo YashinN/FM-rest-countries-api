1be1141c137bd9bde81661f2ce3654124fabe519
Yashin <yashinnaran@gmail.com>
2023-08-31 12:35:56 +0200

    Created CountryPage.js with base html displaying the details of selected country that has been passed via a prop

---
+++ frontend/src/pages/CountryPage.js
@@ -0,0 +1,72 @@
+import styles from "./CountryPage.module.css";
+
+const CountryPage = ({ selectedCountry }) => {
+  const {
+    nativeName,
+    population,
+    region,
+    subregion,
+    capital,
+    topLevelDomain,
+    currencies,
+    languages,
+  } = selectedCountry;
+  return (
+    <main>
+      <section className="container p-0 general_container ">
+        <button className={styles.back_btn}>Back</button>
+        <div className={styles.main_container}>
+          <div
+            className={`container ${styles.img_wrapper}`}
+            style={{ backgroundImage: `url(${selectedCountry.flags.svg})` }}
+          ></div>
+
+          <div className={`containerp-0 ${styles.details_wrapper}`}>
+            <h2 className={styles.country_title}>{selectedCountry.name}</h2>
+            <div className={styles.countryDetails}>
+              <ul className="p-0">
+                <li>
+                  <strong>Native Name:</strong> {nativeName}
+                </li>
+                <li>
+                  <strong>Population:</strong> {population}
+                </li>
+                <li>
+                  <strong>Region:</strong> {region}
+                </li>
+                <li>
+                  <strong>Sub Region:</strong> {subregion}
+                </li>
+                <li>
+                  <strong>Capital:</strong> {capital}
+                </li>
+              </ul>
+
+              <ul className="p-0">
+                <li>
+                  <li>
+                    <strong>Top Level Domain:</strong> {topLevelDomain}
+                  </li>
+                  <li>
+                    <strong>Currencies:</strong>{" "}
+                    {currencies?.map((cur, i) =>
+                      currencies.length === i + 1 ? cur.name : cur.name + ","
+                    )}
+                  </li>
+                  <li>
+                    <strong>Languages:</strong>{" "}
+                    {languages?.map((lan, i) =>
+                      languages.length === i + 1 ? lan.name : lan.name + ","
+                    )}
+                  </li>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default CountryPage;
