dc2cc0bb0d9d219fdf68c28b0154b04e388ce9b7
Yashin <yashinnaran@gmail.com>
2023-09-01 21:04:35 +0200

    useNavigate hook is used to navigate to CountryPage when country card is clicked. This is handled by handleCountryDetails handler.Routes have been restructured to ensure users can use the browser back navigation button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,24 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+  Navigate,
+} from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import HomePage from "./pages/HomePage";
 import CountryPage from "./pages/CountryPage";
 import TempData from "./data.json";
 
 function App() {
+  const navigate = useNavigate();
   const [darkMode, setDarkMode] = useState(true);
   const [countryDetails, setCountryDetails] = useState(null);
 
   const handleCountryDetails = (country) => {
     setCountryDetails(country);
+    navigate(`/country/${country.name}`);
   };
 
   useEffect(() => {
@@ -20,43 +28,33 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Navbar darkMode={darkMode} />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              !countryDetails ? (
-                <HomePage
-                  TempData={TempData}
-                  handleCountryDetails={handleCountryDetails}
-                  darkMode={darkMode}
-                />
-              ) : (
-                <Navigate to="/country" />
-              )
-            }
-          />
+      <Navbar darkMode={darkMode} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomePage
+              TempData={TempData}
+              handleCountryDetails={handleCountryDetails}
+              darkMode={darkMode}
+            />
+          }
+        />
 
-          <Route
-            path="/country"
-            element={
-              !countryDetails ? (
-                <Navigate to="/" />
-              ) : (
-                <CountryPage
-                  selectedCountry={countryDetails}
-                  setCountryDetails={setCountryDetails}
-                  TempData={TempData}
-                  darkMode={darkMode}
-                />
-              )
-            }
-          />
+        <Route
+          path="/country/:id"
+          element={
+            <CountryPage
+              selectedCountry={countryDetails}
+              setCountryDetails={setCountryDetails}
+              TempData={TempData}
+              darkMode={darkMode}
+            />
+          }
+        />
 
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </BrowserRouter>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </>
   );
 }
