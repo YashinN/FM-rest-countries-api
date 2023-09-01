c68a78f978f3cc9281149455532f929bc23c7dd2
Yashin <yashinnaran@gmail.com>
2023-09-01 13:09:38 +0200

    Navbar component get darkMode prop toggles color of typography light/dark.This is done using global css ids attatched to elements that need to change color

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,13 +1,16 @@
 import styles from "./Navbar.module.css";
 import MoonIcon from "../Icons/MoonIcon";
 
-const Navbar = () => {
+const Navbar = ({ darkMode }) => {
   return (
-    <nav className={`${styles.navbar}`}>
+    <nav id={darkMode ? "darkTyp" : "lightTyp"} className={`${styles.navbar}`}>
       <div className={styles.nav_container}>
         <h1 className={`${styles.nav_title}  mb-0`}>Where in the world?</h1>
-        <button className={`${styles.theme_btn}`}>
-          <MoonIcon /> Dark Mode
+        <button
+          id={darkMode ? "darkTyp" : "lightTyp"}
+          className={`${styles.theme_btn}`}
+        >
+          <MoonIcon darkMode={darkMode} /> Dark Mode
         </button>
       </div>
     </nav>
