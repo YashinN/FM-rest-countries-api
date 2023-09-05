9a3f8bcb22838e4252a623c9a7d6e013da46effc
Yashin <yashinnaran@gmail.com>
2023-09-05 16:06:09 +0200

    Added framer-motion animations to navabr h1 and theme button ,these animation run when component mounts.

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,21 +1,42 @@
 import styles from "./Navbar.module.css";
 import MoonIcon from "../Icons/MoonIcon";
+import { motion } from "framer-motion";
+
+const headerVariants = {
+  hidden: { x: "-100vw" },
+  visible: { x: 0, transition: { duration: 0.7, type: "spring" } },
+};
+
+const buttonVariants = {
+  hidden: { x: "100vw" },
+  visible: { x: 0, transition: { duration: 0.5, type: "spring" } },
+};
 
 const Navbar = ({ darkMode, handleDarkMode }) => {
   return (
     <nav id={darkMode ? "dark" : "light"} className={`${styles.navbar}`}>
       <div className={styles.nav_container}>
-        <h1 className={`${styles.nav_title}  mb-0`}>Where in the world?</h1>
-        <button
+        <motion.h1
+          className={`${styles.nav_title}  mb-0`}
+          variants={headerVariants}
+          animate="visible"
+          initial="hidden"
+        >
+          Where in the world?
+        </motion.h1>
+        <motion.button
           id={darkMode ? "dark" : "light"}
           className={`${styles.theme_btn}`}
           onClick={handleDarkMode}
+          variants={buttonVariants}
+          animate="visible"
+          initial="hidden"
         >
           <span className={styles.moon_icon}>
             <MoonIcon darkMode={darkMode} />
           </span>
           Dark Mode
-        </button>
+        </motion.button>
       </div>
     </nav>
   );
