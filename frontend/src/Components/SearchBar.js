6867ba97036400c9473cedef6bd475f1d916c5ac
Yashin <yashinnaran@gmail.com>
2023-09-06 09:32:53 +0200

    Framer motion hover animation added tosearch bar.

--- frontend/src/Components/SearchBar.js
+++ frontend/src/Components/SearchBar.js
@@ -1,21 +1,29 @@
 import styles from "./SearchBar.module.css";
 import SearchIcon from "../Icons/SearchIcon";
+import { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 
 const searchVariant = {
   hidden: { x: "-100vw" },
   visible: { x: 0, transition: { duration: 0.7, type: "spring", damping: 10 } },
+  hover: {
+    scale: 1.1,
+  },
 };
 
 const SearchBar = ({ onSearch, searchQuery, darkMode }) => {
+  const ref = useRef(null);
+
   return (
     <motion.div
       className={styles.search_container}
       variants={searchVariant}
       animate="visible"
       initial="hidden"
+      whileHover="hover"
     >
-      <input
+      <motion.input
+        ref={ref}
         id={darkMode ? "dark" : "light"}
         className={`${darkMode ? "search_input_dark" : "search_input_light"} ${
           styles.search_bar
@@ -26,9 +34,9 @@ const SearchBar = ({ onSearch, searchQuery, darkMode }) => {
         onChange={(e) => onSearch(e.target.value)}
       />
 
-      <span className={styles.icon_container}>
+      <motion.span className={styles.icon_container}>
         <SearchIcon darkMode={darkMode} />
-      </span>
+      </motion.span>
     </motion.div>
   );
 };
