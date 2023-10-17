35a0f1668ccd29c28f8222eba4439e7c8d7c618a
Yashin <yashinnaran@gmail.com>
2023-10-17 11:48:09 +0200

    The details of a country has been moved into a seperate component from CountryPage.js to CountryDetails

---
+++ frontend/src/Components/CountryDetails.js
@@ -0,0 +1,70 @@
+import { motion } from "framer-motion";
+
+const CountryDetails = ({ styles, selectedCountry }) => {
+  const {
+    nativeName,
+    population,
+    region,
+    subregion,
+    capital,
+    topLevelDomain,
+    currencies,
+    languages,
+    borders,
+    flags,
+  } = selectedCountry[0];
+  return (
+    <>
+      <h2 className={styles.country_title}>{selectedCountry.name}</h2>
+      <div className={styles.countryDetails}>
+        <ul className="p-0">
+          <li>
+            <strong>Native Name:</strong> {nativeName}
+          </li>
+          <li>
+            <strong>Population:</strong>
+            {String(population)
+              .split("")
+              .reverse()
+              .map((num, i) =>
+                i === 0 ? num : (i + 1) % 3 === 0 ? "," + num : num
+              )
+              .reverse()
+              .join("")}
+          </li>
+          <li>
+            <strong>Region:</strong> {region}
+          </li>
+          <li>
+            <strong>Sub Region:</strong> {subregion}
+          </li>
+          <li>
+            <strong>Capital:</strong> {capital}
+          </li>
+        </ul>
+
+        <ul className="p-0">
+          <li>
+            <li>
+              <strong>Top Level Domain:</strong> {topLevelDomain}
+            </li>
+            <li>
+              <strong>Currencies:</strong>{" "}
+              {currencies?.map((cur, i) =>
+                currencies.length === i + 1 ? cur.name : cur.name + ","
+              )}
+            </li>
+            <li>
+              <strong>Languages:</strong>{" "}
+              {languages?.map((lan, i) =>
+                languages.length === i + 1 ? lan.name : lan.name + ","
+              )}
+            </li>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default CountryDetails;
--- frontend/src/pages/CountryPage.js
+++ frontend/src/pages/CountryPage.js
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import styles from "./CountryPage.module.css";
 import BackArrow from "../Icons/BackArrow";
 import BorderCountries from "../Components/BorderCountries";
+import CountryDetails from "../Components/CountryDetails";
 
 const backVariant = {
   hidden: { y: "-100vw" },
@@ -43,23 +44,22 @@ const detailsVariant = {
 
 const CountryPage = ({ TempData, darkMode }) => {
   const navigate = useNavigate();
-
   const { id } = useParams();
 
   const selectedCountry = TempData.filter((country) => country.name === id);
 
-  const {
-    nativeName,
-    population,
-    region,
-    subregion,
-    capital,
-    topLevelDomain,
-    currencies,
-    languages,
-    borders,
-    flags,
-  } = selectedCountry[0];
+  // const {
+  //   nativeName,
+  //   population,
+  //   region,
+  //   subregion,
+  //   capital,
+  //   topLevelDomain,
+  //   currencies,
+  //   languages,
+  //   borders,
+  //   flags,
+  // } = selectedCountry[0];
 
   const handleBorderCountry = (code) => {
     const country = TempData.filter((country) => country.alpha3Code === code);
@@ -94,14 +94,14 @@ const CountryPage = ({ TempData, darkMode }) => {
             darkMode ? "country_details_dark" : "country_details_light"
           } ${styles.main_container}`}
         >
-          <motion.div
+          {/* <motion.div
             className={`container p-0 ${styles.img_wrapper}`}
             variants={imageVariant}
             animate="visible"
             initial="hidden"
           >
             <img className={styles.country_img} src={flags.svg} alt="" />
-          </motion.div>
+          </motion.div> */}
 
           <motion.div
             className={`container p-0 ${styles.details_wrapper}`}
@@ -109,61 +109,15 @@ const CountryPage = ({ TempData, darkMode }) => {
             animate="visible"
             initial="hidden"
           >
-            <h2 className={styles.country_title}>{selectedCountry.name}</h2>
-            <div className={styles.countryDetails}>
-              <ul className="p-0">
-                <li>
-                  <strong>Native Name:</strong> {nativeName}
-                </li>
-                <li>
-                  <strong>Population:</strong>
-                  {String(population)
-                    .split("")
-                    .reverse()
-                    .map((num, i) =>
-                      i === 0 ? num : (i + 1) % 3 === 0 ? "," + num : num
-                    )
-                    .reverse()
-                    .join("")}
-                </li>
-                <li>
-                  <strong>Region:</strong> {region}
-                </li>
-                <li>
-                  <strong>Sub Region:</strong> {subregion}
-                </li>
-                <li>
-                  <strong>Capital:</strong> {capital}
-                </li>
-              </ul>
+            <CountryDetails styles={styles} selectedCountry={selectedCountry} />
 
-              <ul className="p-0">
-                <li>
-                  <li>
-                    <strong>Top Level Domain:</strong> {topLevelDomain}
-                  </li>
-                  <li>
-                    <strong>Currencies:</strong>{" "}
-                    {currencies?.map((cur, i) =>
-                      currencies.length === i + 1 ? cur.name : cur.name + ","
-                    )}
-                  </li>
-                  <li>
-                    <strong>Languages:</strong>{" "}
-                    {languages?.map((lan, i) =>
-                      languages.length === i + 1 ? lan.name : lan.name + ","
-                    )}
-                  </li>
-                </li>
-              </ul>
-            </div>
-
+            {/* 
             <BorderCountries
               styles={styles}
               onBorderCountry={handleBorderCountry}
               borderCountries={borders}
               darkMode={darkMode}
-            />
+            /> */}
           </motion.div>
         </div>
       </section>
