3e35f70258fb98c0e939709f50f723d380beb611
Yashin <yashinnaran@gmail.com>
2023-10-20 16:45:53 +0200

    removed dotenv.

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,15 @@
 const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
 
 const cors = require("cors");
-const connectDB = require("./config/mongoConnect");
-
-const countryRoutes = require("./routes/countryRoutes");
 
 const PORT = process.env.PORT || 6000;
 const app = express();
 // connectDB();
 
 app.use(cors());
-app.use("/api", countryRoutes);
+app.get("/api", (req, res) => {
+  res.json("Helooo");
+});
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `);
--- backend/routes/countryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-// const { getAllCountries } = require("../controllers/countriesController");
-
-const router = express.Router();
-
-// router.get("/countries", getAllCountries);
-
-module.exports = router;
