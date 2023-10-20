43d74d324e6029ec7ca267cad2a2b65c840eb5f3
Yashin <yashinnaran@gmail.com>
2023-10-20 15:57:02 +0200

    added mongo db connection.

--- backend/config/mongoConnect.js
+++ backend/config/mongoConnect.js
@@ -1,13 +1,15 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const connectDB = async () => {
-//   try {
-//     const connect = await mongoose.connect(process.env.MONGO_URI);
-//     console.log("Mongo db connected");
-//   } catch (err) {
-//     console.log(err);
-//     process.exit(1);
-//   }
-// };
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(
+      "mongodb+srv://yashinnaran:G5COrwgQnYHinlaP@countiesdb.in16qug.mongodb.net/?retryWrites=true&w=majority"
+    );
+    console.log("Mongo db connected");
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
 
-// module.exports = connectDB;
+module.exports = connectDB;
