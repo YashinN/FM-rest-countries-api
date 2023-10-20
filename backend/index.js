4c93e99119a4652fb99ba61e41b76d0e1c38861d
Yashin <yashinnaran@gmail.com>
2023-10-20 16:48:40 +0200

    added mongo connection.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+
 const cors = require("cors");
+
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(
+      "mongodb+srv://yashinnaran:G5COrwgQnYHinlaP@countiesdb.in16qug.mongodb.net/countriesDB?retryWrites=true&w=majority"
+    );
+    console.log("Mongo DB Connected");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 const PORT = process.env.PORT || 6000;
 const app = express();
@@ -14,6 +28,8 @@ app.get("/api", (req, res) => {
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `);
 });
+
+connectDB();
 
 // Export the Express AP
 module.exports = app;
