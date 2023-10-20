44807eaef385d9107d5fc03d6a3d2131d129157b
Yashin <yashinnaran@gmail.com>
2023-10-20 16:34:51 +0200

    test mongo connection.

--- backend/index.js
+++ backend/index.js
@@ -7,27 +7,20 @@ const connectDB = require("./config/mongoConnect");
 
 const countryRoutes = require("./routes/countryRoutes");
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 6000;
 const app = express();
 // connectDB();
 
 app.use(cors());
 app.use("/api", countryRoutes);
 
-// app.listen(PORT, () => {
-//   console.log(`API listening on PORT ${PORT} `);
-// });
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
+});
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`server running on port ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+mongoose.connect(process.env.MONGO_URI).catch((error) => {
+  console.log(error);
+});
 
 // Export the Express AP
 module.exports = app;
