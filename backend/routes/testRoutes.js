d8f6e87dc3a15c391507036b5465c11b70430148
Yashin <yashinnaran@gmail.com>
2023-10-20 10:02:33 +0200

    res in json.

--- backend/routes/testRoutes.js
+++ backend/routes/testRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/now", (req, res) => {
-  res.send("Hellow !!!");
+  res.json("Hellow !!!");
 });
 
 module.exports = router;
