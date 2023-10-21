374fdd98ffd1449ad5533bf5ab1094aef7eb3a63
Yashin <yashinnaran@gmail.com>
2023-10-21 09:44:22 +0200

    controller responds with test json from mongoDB.

--- backend/controllers/countriesController.js
+++ backend/controllers/countriesController.js
@@ -3,7 +3,7 @@ const Country = require("../models/countriesModel");
 const getAllCountries = async (req, res) => {
   try {
     const countries = await Country.find({});
-    res.json("This is working!!!");
+    res.json(countries);
   } catch (err) {
     console.log(err);
   }
