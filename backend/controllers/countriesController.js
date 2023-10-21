3d4c8128873da8f7dd981e0fcc8deeddc39b40f0
Yashin <yashinnaran@gmail.com>
2023-10-21 18:29:06 +0200

    adjusted mongo db query to fix limit.

--- backend/controllers/countriesController.js
+++ backend/controllers/countriesController.js
@@ -2,7 +2,7 @@ const Country = require("../models/countriesModel");
 
 const getAllCountries = async (req, res) => {
   try {
-    const countries = await Country.find({}).limit(20);
+    const countries = await Country.find().limit(20);
     res.json(countries);
   } catch (err) {
     console.log(err);
