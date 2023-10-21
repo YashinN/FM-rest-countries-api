be5745aa43e201f0cd18b431fbc9f8b05a67e60b
Yashin <yashinnaran@gmail.com>
2023-10-21 09:13:36 +0200

    .env created, verscel json adjusted to use vercel node , mongo connection starts directly in idex.js.

--- backend/index.js
+++ backend/index.js
@@ -3,26 +3,24 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const connectDB = async () => {
-  try {
-    const connect = await mongoose.connect(
-      "mongodb+srv://yashinnaran:G5COrwgQnYHinlaP@countiesdb.in16qug.mongodb.net/countriesDB?retryWrites=true&w=majority"
-    );
-    console.log("Mongo DB Connected");
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-};
-
 const app = express();
 const PORT = 4000;
-connectDB();
 
 app.use(cors());
+
 app.get("/api", (req, res) => {
   res.json("Helooo");
 });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Mongo DB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `);
