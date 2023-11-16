3988e2d232fba75024bacaf193b256a0df1a5f9a
Yashin <yashinnaran@gmail.com>
2023-11-16 17:28:09 +0200

    Reduced number of properties being sent from backend, requests only include properties needed in frontend.

--- backend/controllers/countriesController.js
+++ backend/controllers/countriesController.js
@@ -1,8 +1,24 @@
 const Country = require("../models/countriesModel");
 
+const properties = {
+  name: 1,
+  flags: 1,
+  population: 1,
+  region: 1,
+  capital: 1,
+  topLevelDomain: 1,
+  borders: 1,
+  languages: 1,
+  nativeName: 1,
+  subregion: 1,
+  currencies: 1,
+  alpha3Code: 1,
+};
+
 const getAllCountries = async (req, res) => {
   try {
-    const countries = await Country.find();
+    const countries = await Country.find({}, { ...properties });
+
     res.json(countries);
   } catch (err) {
     console.log(err);
@@ -12,7 +28,10 @@ const getAllCountries = async (req, res) => {
 const getCountry = async (req, res) => {
   try {
     const { name: countryName } = req.params;
-    const country = await Country.find({ name: countryName });
+    const country = await Country.find(
+      { name: countryName },
+      { ...properties }
+    );
     res.json(country);
   } catch (err) {
     console.log("error");
